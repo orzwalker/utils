f42f0f7fecef8a71dd389159ad6339e808b92606
walker <orzwalker@gmail.com>
2022-11-21 11:50:36 +0800

    init project

---
+++ src/main/java/org/example/UtilsApplication.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author walker
+ * @since 2022/11/21 11:21
+ */
+@SpringBootApplication
+public class UtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtilsApplication.class, args);
+    }
+}
---
+++ src/main/java/org/example/aspectLog/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 切面日志记录
+ *
+ *
+ * @author walker
+ * @since 2022/11/21 11:25
+ */
+package org.example.aspectLog;
